af11a113a72d70d6454c259c4766e0272613e03b
Jamie <Sigton@users.noreply.github.com>
2017-11-08 13:15:54 +0000

    Add files via upload

---
+++ messenger.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+import json
+
+data = {}
+
+name = input("Enter your username: ")
+
+with open("//H023FILESRV01/OldPupilSHare/messages.json", 'r') as infile:
+    data = json.load(infile)
+
+def send_message(message):
+
+    global data, name
+
+    data["messages"].append("{}: {}".format(name, message))
+
+    with open("//H023FILESRV01/OldPupilSHare/messages.json", 'w') as outfile:
+        json.dump(data, outfile)
+
+    refresh()
+
+def refresh():
+
+    global data, display
+
+    with open("//H023FILESRV01/OldPupilSHare/messages.json", 'r') as infile:
+        data = json.load(infile)
+
+    display.delete('1.0', tk.END)
+
+    for message in data["messages"]:
+        display.insert(tk.END, message + "\n")
+
+root = tk.Tk()
+entry = tk.Entry(root)
+entry.pack()
+send_button = tk.Button(root, text="Send", command=lambda: send_message(entry.get()))
+send_button.pack()
+refresh_button = tk.Button(root, text="Refresh", command=refresh)
+refresh_button.pack()
+display = tk.Text(root)
+display.pack()
+send_message("{} has joined!".format(name))
+
+root.mainloop()

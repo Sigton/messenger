97b4b83328657633382dd08124ac120d4dff92a8
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-08 17:38:22 +0000

    Making the gui properly

--- main.py
+++ main.py
@@ -3,11 +3,55 @@ import json
 
 from settings import *
 
+'''
 name = input("Enter your username: ")
 
 with open(FILE_PATH, 'r') as infile:
     data = json.load(infile)
+'''
 
+
+class Messenger(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title(self, "Messenger")
+        self.geometry('800x600')
+
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for f in (MainPage,):
+
+            frame = f(self.container, self)
+
+            self.frames[f] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(MainPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class MainPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.controller = controller
 
 def send_message(message):
 

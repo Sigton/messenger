90d10d3eefb0592b7b3a5edc3c6556f01867f2f9
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-11 09:15:18 +0000

    Changing things

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ class MainPage(tk.Frame):
 
         self.menu_bar.add_command(label="Quit", command=self.logoff)
         self.menu_bar.add_command(label="Logout", command=None)
-        self.menu_bar.add_command(label="Servers", command=None)
+        self.menu_bar.add_command(label="Servers", command=self.open_server_settings)
         self.menu_bar.add_command(label="Styling", command=None)
 
     def setup(self):

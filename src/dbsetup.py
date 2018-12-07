52b937073ab49e3d3fed13429c9f6477801ba407
Jamie <Sigton@users.noreply.github.com>
2018-12-07 11:56:26 +0000

    Add files via upload

---
+++ src/dbsetup.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+# "//H023FILESRV01/OldPupilSHare/slamjam/messenger/defaultserver.db")
+
+#commented out to avoid accidentally running
+'''
+db = sqlite3.connect("//h023filesrv01/OldPupilSHare/gamersquad/wpservers/general.db")
+
+cursor = db.cursor()
+
+try:
+    cursor.execute("""
+        DROP TABLE messages
+    """)
+    cursor.execute("""
+        DROP TABLE users
+    """)
+except:
+    pass
+
+cursor.execute("""
+    CREATE TABLE messages(id INTEGER PRIMARY KEY, time TEXT, name TEXT, message TEXT, prefix INTEGER, colour TEXT)
+""")
+
+cursor.execute("""
+    CREATE TABLE users(id INTEGER PRIMARY KEY, nickname TEXT, status INTEGER)
+""")
+
+db.commit()
+
+
+db.close()
+'''
